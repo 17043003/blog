0888cdb8ca6db8c39d856078bcda5223d56d9ad6
石塚昌也 <ishizuka@MacBook-Pro.local>
2021-04-04 19:54:23 +0900

    Add firebase plugin

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/firebase'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -72,15 +72,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-
-  publicRuntimeConfig: {},
-  privateRuntimeConfig: {
-    firebaseProjectID: process.env.FIREBASE_PROJECT_ID,
-    firebaseApiKey: process.env.FIREBASE_API_KEY,
-    firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    firebaseAppId: process.env.FIREBASE_APP_ID,
-    firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
-  },
 }
--- front/plugins/firebase.js
+++ front/plugins/firebase.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
 
 import 'firebase/auth'
 import 'firebase/firestore'
 
 const config = {
-  projectId: this.$config.firebaseProjectId,
-  apiKey: this.$config.firebaseApiKey,
-  authDomain: this.$config.firebaseAuthDomain,
-  storageBucket: this.$config.firebaseStorageBucket,
-  messagingSenderId: this.$config.firebaseMessagingSenderId,
-  appId: this.$config.firebaseAppId,
-  measurementId: this.$config.firebaseMeasurementId,
+  apiKey: 'AIzaSyBhrBiqZzHJ5JP9IpJ7c9wmUMZGkZLrM_s',
+  authDomain: 'myblog-f0a4b.firebaseapp.com',
+  projectId: 'myblog-f0a4b',
+  storageBucket: 'myblog-f0a4b.appspot.com',
+  messagingSenderId: '218438676844',
+  appId: '1:218438676844:web:5efb6fa915ce62489a8349',
+  measurementId: 'G-JGZLXBG1M5',
 }
 
 // 二重の初期化を防ぐ
