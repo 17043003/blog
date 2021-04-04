297b0216e6f2072cc4ff14df222e24f09e129e77
石塚昌也 <ishizuka@MacBook-Pro.local>
2021-04-04 19:58:08 +0900

    Improve header link name

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -7,8 +7,8 @@
       ></v-avatar>
 
       <v-tabs centered class="ml-n9" color="grey darken-1">
-        <v-tab v-for="link in links" :key="link">
-          {{ link }}
+        <v-tab v-for="(link, key) in links" :key="link" nuxt :to="link">
+          {{ key }}
         </v-tab>
       </v-tabs>
 
@@ -22,13 +22,7 @@
     <v-main class="grey lighten-3">
       <v-container>
         <v-row>
-          <v-col cols="12" sm="2">
-            <v-sheet rounded="lg" min-height="268">
-              <!--  -->
-            </v-sheet>
-          </v-col>
-
-          <v-col cols="12" sm="8">
+          <v-col cols="12" sm="10">
             <v-sheet min-height="70vh" rounded="lg">
               <Nuxt class="main-content" />
             </v-sheet>
@@ -48,7 +42,12 @@
 <script>
 export default {
   data: () => ({
-    links: ['Top', 'Posts', 'Profile', 'Updates'],
+    links: {
+      top: '/',
+      posts: '/posts',
+      profile: '/profile',
+      updates: '/updates',
+    },
   }),
 }
 </script>
