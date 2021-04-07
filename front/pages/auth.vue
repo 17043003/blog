4449aac0db400e0b9585585ad4fd224275071f65
石塚昌也 <ishizuka@MacBook-Pro.local>
2021-04-08 00:07:33 +0900

    Add signout method

---
+++ front/pages/auth.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button @click="signin()">ログイン</button>
+    <button @click="signout()">ログアウト</button>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+export default {
+  methods: {
+    signin() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          alert(result)
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          alert(error)
+        })
+    },
+    signout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          alert('signout succeeded')
+        })
+        .catch((error) => {
+          this.signoutError = error
+          alert(error)
+        })
+    },
+  },
+}
+</script>
