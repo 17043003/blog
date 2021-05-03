b31c253b53cc89187b2266c38f13e19229f67124
石塚昌也 <ishizuka@MacBook-Pro.local>
2021-05-04 04:40:57 +0900

    Add: menu svg to default header on mobile

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,22 +1,14 @@
 <template>
   <v-app id="inspire">
     <v-app-bar app color="white" flat>
-      <v-avatar
-        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
-        size="32"
-      ></v-avatar>
-
       <v-tabs v-if="!isMobile" centered class="ml-n9" color="grey darken-1">
         <v-tab v-for="(link, key) in loggedInLinks" :key="link" nuxt :to="link">
           {{ key }}
         </v-tab>
       </v-tabs>
-
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey darken-1 shrink"
-        size="32"
-      ></v-avatar>
+      <div v-else>
+        <MenuSvg />
+      </div>
     </v-app-bar>
 
     <v-main class="grey lighten-3">
@@ -40,7 +32,12 @@
 </template>
 
 <script>
+import MenuSvg from '@/assets/format_list_bulleted_black_24dp.svg'
+
 export default {
+  components: {
+    MenuSvg,
+  },
   data: () => ({
     links: {
       top: '/',
