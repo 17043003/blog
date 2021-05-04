dedb0ef71b30d340882573c12e7f59cc884a0e9b
石塚昌也 <ishizuka@MacBook-Pro.local>
2021-05-05 08:26:43 +0900

    Improve: delete button is hidden when not logged in

--- front/pages/posts/_id/index.vue
+++ front/pages/posts/_id/index.vue
@@ -3,7 +3,7 @@
     <h3 class="create-date date">作成日：{{ createdDate }}</h3>
     <h3 class="update-date date">更新日：{{ updatedDate }}</h3>
     <nuxt-link v-if="isLoggedIn" nuxt append to="edit">編集</nuxt-link>
-    <button @click="pushDeleteButton()">削除</button>
+    <button v-if="isLoggedIn" @click="pushDeleteButton()">削除</button>
     <h1>{{ post.title }}</h1>
     <MarkDown :content="post.content"></MarkDown>
   </div>
